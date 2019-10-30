579c3442377ef600bd0c5ca7e18b12c66e9f7e51
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-10-30 12:16:06 +0700

    Добавляет плеер

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,103 @@ import WelcomeScreen from "../../components/welcome-screen/welcome-screen.jsx";
 import QuestionArtist from "../../components/question-artist/question-artist.jsx";
 import QuestionGenre from "../../components/question-genre/question-genre.jsx";
 
+
+/*const Type = {
+  ARTIST: `game--artist`,
+  GENRE: `game--genre`,
+};
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: -1,
+    };
+  }
+
+  _getScreen(question, onClick) {
+    if (!question) {
+      const {
+        errorCount,
+        gameTime,
+      } = this.props;
+
+      return <WelcomeScreen
+        errorCount={errorCount}
+        gameTime={gameTime}
+        onClick={onClick}
+      />;
+    }
+
+    switch (question.type) {
+      case `genre`: return <GenreQuestionScreen
+        question={question}
+        onAnswer={onClick}
+      />;
+
+      case `artist`: return <ArtistQuestionScreen
+        question={question}
+        onAnswer={onClick}
+      />;
+    }
+
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+    const {question} = this.state;
+
+    return <section className={`game ${Type.ARTIST}`}>
+      {this.state.question !== -1 && <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+                  style={{
+                    filter: `url(#blur)`,
+                    transform: `rotate(-90deg) scaleY(-1)`,
+                    transformOrigin: `center`
+                  }}
+          />
+        </svg>
+
+        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+          <span className="timer__mins">05</span>
+          <span className="timer__dots">:</span>
+          <span className="timer__secs">00</span>
+        </div>
+
+        <div className="game__mistakes">
+          <div className="wrong"/>
+          <div className="wrong"/>
+          <div className="wrong"/>
+        </div>
+      </header>}
+
+      {this._getScreen(questions[question], () => {
+        this.setState({
+          question: question + 1 >= questions.length
+            ? -1
+            : question + 1,
+        });
+      })}
+    </section>;
+  }
+}
+
+
+App.propTypes = {
+  errorCount: PropTypes.number.isRequired,
+  gameTime: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
+*/
+
 class App extends React.PureComponent {
   static getScreen(question, props, onUserAnswer) {
     if (question === -1) {
--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -4,19 +4,19 @@ const questions = [
     genre: `rock`,
     answers: [
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Leona_Lewis_-_I_See_You_(OST_Avatar).mp3`,
         genre: `rock`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Celine_Dion_-_My_Heart_Will_Go_On_(OST_Titanic).mp3`,
         genre: `pop`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/rus/low/Nautilus_Pompilius_-_Mater_bogov_(OST_Brat).mp3`,
         genre: `jazz`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Pirates_of_the_Caribbean_-_He-s_a_Pirate.mp3`,
         genre: `rock`,
       },
     ],
@@ -26,19 +26,19 @@ const questions = [
     genre: `folk`,
     answers: [
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/rus/low/Okudzhava_Bulat_-_Pesnya_Vereschagina_(Vashe_blagorodie_OST_Beloe_solnce_pustyni).mp3`,
         genre: `rock`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Avril_Lavigne_-_Alice.mp3`,
         genre: `pop`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Pirates_of_the_Caribbean_-_The_Black_Pearl_(Chernaya_Zhemchuzhina).mp3`,
         genre: `jazz`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://sefan.ru/music/files/eng/low/Roy_Orbison_-_Oh_Pretty_Woman.mp3`,
         genre: `rock`,
       },
     ],
@@ -47,7 +47,7 @@ const questions = [
     type: `artist`,
     song: {
       artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://sefan.ru/music/files/eng/low/Leona_Lewis_-_I_See_You_(OST_Avatar).mp3`,
     },
     answers: [
       {
@@ -68,7 +68,7 @@ const questions = [
     type: `artist`,
     song: {
       artist: `Jack Daniels`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://sefan.ru/music/files/eng/low/Roy_Orbison_-_Oh_Pretty_Woman.mp3`,
     },
     answers: [
       {
